0544bca48ddd67a45b7d8cddf50ea09b04fbf638
Daniel Kamiński <69056082+DanielKami@users.noreply.github.com>
2023-06-04 17:40:53 +0200

    Add files via upload

--- PassiveRadar/Cumulate.cs
+++ PassiveRadar/Cumulate.cs
@@ -1,4 +1,6 @@
-﻿namespace PasiveRadar
+﻿using System.Threading.Tasks;
+
+namespace PasiveRadar
 {
     class RadarCumulate
     {
@@ -15,8 +17,8 @@
             Rows = flags.Rows;
             MaxAverage = flags.MaxAverage;
             uint Frame = Columns * Rows;
-            CumulateBuffer = new float[(MaxAverage + 1) * Frame ];
-            
+            CumulateBuffer = new float[(MaxAverage + 1) * Frame];
+
         }
 
         public void Run(float[] data, float[] PostProc, int average)
@@ -34,10 +36,16 @@
             //Cumulation
             if (CCR + Frame > CumulateBuffer.Length)
                 return;
-               
 
-                for (uint i = 0; i < Frame; i++)
-                    CumulateBuffer[CCR + i] = data[i];
+
+            Parallel.For(0, Frame,
+             i =>
+             {
+                 CumulateBuffer[CCR + i] = data[i];
+             });
+
+            //for (uint i = 0; i < Frame; i++)
+            //    CumulateBuffer[CCR + i] = data[i];
 
             //Averaging
             float one_ave = 1.0f / average;
@@ -45,16 +53,35 @@
             //Clera the output
             System.Array.Clear(PostProc, 0, (int)Frame);
 
-            for (uint j = 0; j < average; j++)
-            {
-                long jF = j * Frame;
-                for (uint i = 0; i < Frame; i++)
-                    PostProc[i] += CumulateBuffer[i + jF];
-            }
+            //for (uint j = 0; j < average; j++)
+            //{
+            //    long jF = j * Frame;
+            //    for (uint i = 0; i < Frame; i++)
+            //        PostProc[i] += CumulateBuffer[i + jF];
+            //}
 
+            Parallel.For(0, average,
+              j =>
+              {
+                  long jF = j * Frame;
+                  for (uint i = 0; i < Frame; i++)
+                  {
+                      PostProc[i] += CumulateBuffer[i + jF];
+
+                  }
+              });
+
+            one_ave *= 255;
             //It is faster to do one more loop
-            for (uint i = 0; i < Frame; i++)
-                PostProc[i] *= 255 * one_ave; //255 is for graphics normalisation
+            //for (uint i = 0; i < Frame; i++)
+            //    PostProc[i] *= one_ave; //255 is for graphics normalisation
+
+            Parallel.For(0, average,
+            i =>
+            {
+                PostProc[i] *= one_ave;
+            });
+
 
         }
     }
